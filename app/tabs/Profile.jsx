b2c50bfa4927bff8ad03ed540266ea993ccea7ce
Rahul garg <rgarg3577@gmail.com>
2024-07-19 18:10:37 +0530

    profile page updated

--- app/tabs/Profile.jsx
+++ app/tabs/Profile.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import React from "react";
 import { icons } from "../../constants";
 import { Link } from "expo-router";
@@ -8,7 +8,7 @@ import { LinearGradient } from "expo-linear-gradient";
 
 export default function Profile() {
   return (
-    <View
+    <ScrollView
       className="flex pt-10 min-h-full"
       style={{ backgroundColor: "#141740" }}
     >
@@ -27,9 +27,12 @@ export default function Profile() {
       <View style={[styles.gradientCircle, styles.circle2]} />
 
       <BlurView
-        intensity={0}
-        className="m-10 flex items-center justify-center w-72 h-80 border-none rounded-3xl shadow-lg"
-        style={{ elevation: 5 }}
+        intensity={90}
+        className="m-10 flex items-center justify-center w-72 h-80 border-none rounded-3xl shadow-2xl"
+        style={{
+          elevation: 3,
+          overflow: "hidden",
+        }}
       >
         <View className="top-10 flex-1">
           <View className="">
@@ -47,7 +50,59 @@ export default function Profile() {
           </Text>
         </View>
       </BlurView>
-    </View>
+      <View>
+        <View className="ml-2 mb-1">
+          <Text className="text-2xl font-bold text-gray-400">
+            Personal Information
+          </Text>
+          <View className="flex-row mt-5">
+            <View className="m-2 mr-5">
+              <Image source={icons.calender} className="w-7 h-9" />
+            </View>
+            <View>
+              <Text className="text-white text-xl">October 31, 2001</Text>
+              <Text className="text-gray-400 font-light text-base">
+                Birthday
+              </Text>
+            </View>
+          </View>
+          <View className="flex-row mt-5">
+            <View className="m-2 mr-5">
+              <Image source={icons.user} className="w-7 h-9" />
+            </View>
+            <View>
+              <Text className="text-white text-xl">Male</Text>
+              <Text className="text-gray-400 font-light text-base">Gender</Text>
+            </View>
+          </View>
+        </View>
+        <View className="h-64 ml-2">
+          <Text className="text-2xl font-bold text-gray-400">
+            College Information
+          </Text>
+          <View className="flex-row mt-5">
+            <View className="m-2 mr-5">
+              <Image source={icons.marker} className="w-9 h-9" />
+            </View>
+            <View>
+              <Text className="text-white text-xl">23EUCIT200</Text>
+              <Text className="text-gray-400 font-light text-base">
+                Roll No.
+              </Text>
+            </View>
+          </View>
+          <View className="flex-row mt-5">
+            <View className="m-2 mr-5">
+              <Image source={icons.home} className="w-9 h-9" />
+            </View>
+            <View>
+              <Text className="text-white text-xl">Development</Text>
+              <Text className="text-gray-400 font-light text-base">Gender</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -61,11 +116,11 @@ const styles = StyleSheet.create({
     opacity: 0.5,
   },
   circle1: {
-    top: 100,
+    top: 75,
     right: -90,
   },
   circle2: {
-    bottom: 200,
+    top: 250,
     left: -60,
   },
 });
--- constants/icons.js
+++ constants/icons.js
@@ -8,6 +8,10 @@ import settings from '../assets/settings.png';
 import back from '../assets/back-button.png';
 import camera from '../assets/camera.png';
 import person from '../assets/person.png';
+import calender from '../assets/calender.png';
+import user from '../assets/user.png';
+import marker from '../assets/marker.png';
+import home from '../assets/home.png';
 
 export default {
     eye,
@@ -20,4 +24,8 @@ export default {
     settings,
     camera,
     person,
+    calender,
+    user,
+    marker,
+    home,
 }
